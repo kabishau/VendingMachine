25c7ae0d33adece349f75449f1ce9127faf6ac2b
AlekseyK <aleksey.kabishau@gmail.com>
2018-12-08 23:46:23 -0500

    defined protocols for data model

---
+++ VendingMachine/VendingMachine.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+enum VendingSelection {
+    case soda
+    case dietSoda
+    case chips
+    case cookie
+    case sandwich
+    case wrap
+    case candyBar
+    case popTart
+    case water
+    case fruitJuice
+    case sportsDrink
+    case gum
+}
+
+protocol VendingItem {
+    var quantity: Int { get set }
+    var price: Double { get }
+}
+
+protocol VendingMachine {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+    
+    init(inventory: [VendingSelection: VendingItem])
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws
+    func deposit(_ amount: Double)
+}
